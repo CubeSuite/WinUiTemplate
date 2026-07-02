c91c7f410ea6a032db593bde938913a80fa22269
Elliot <cubesuite@outlook.com>
2026-07-02 19:40:03 +0100

    Added implicit bool operators to Result records

--- WinUiTemplate.Core/Services/FileUtils/Records.cs
+++ WinUiTemplate.Core/Services/FileUtils/Records.cs
@@ -1,16 +1,39 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.Storage;
 
-namespace WinUiTemplate.Services
+namespace FactoryPlanner.Services
 {
-    public record FileResult(bool Success, string? ErrorMessage, StorageFile? File);
-    public record FilesResult(bool Success, string? ErrorMessage, IReadOnlyList<StorageFile>? Files);
-    public record FileReadResult(bool Success, string? ErrorMessage, string? Content);
-    public record FileWriteResult(bool Success, string? ErrorMessage);
-    public record FolderResult(bool Success, string? ErrorMessage, StorageFolder? Folder);
-    public record OperationResult(bool Success, string? ErrorMessage, bool Notify);
+    public record FileResult(bool Success, string? ErrorMessage, StorageFile? File)
+    {
+        public static implicit operator bool(FileResult result) => result.Success;
+    }
+
+    public record FilesResult(bool Success, string? ErrorMessage, IReadOnlyList<StorageFile>? Files)
+    {
+        public static implicit operator bool(FilesResult result) => result.Success;
+    }
+
+    public record FileReadResult(bool Success, string? ErrorMessage, string? Content)
+    {
+        public static implicit operator bool(FileReadResult result) => result.Success;
+    }
+
+    public record FileWriteResult(bool Success, string? ErrorMessage)
+    {
+        public static implicit operator bool(FileWriteResult result) => result.Success;
+    }
+
+    public record FolderResult(bool Success, string? ErrorMessage, StorageFolder? Folder)
+    {
+        public static implicit operator bool(FolderResult result) => result.Success;
+    }
+
+    public record OperationResult(bool Success, string? ErrorMessage, bool Notify)
+    {
+        public static implicit operator bool(OperationResult result) => result.Success;
+    }
 }
