f0f3ab449c50a9901c177dc4cf938838a2d8f810
Elliot <cubesuite@outlook.com>
2026-07-06 22:05:31 +0100

    Fixed GetRelativePath Bug - Closes #101

--- WinUiTemplate.Core/Services/FileUtils/FileUtils.cs
+++ WinUiTemplate.Core/Services/FileUtils/FileUtils.cs
@@ -176,11 +176,13 @@ namespace WinUiTemplate.Core.Services
         }
 
         public string GetRelativePath(string rootPath, string fullPath) {
+            if (!rootPath.EndsWith("\\") && !rootPath.EndsWith("/")) rootPath += "/";
+
             if (!fullPath.StartsWith(rootPath, StringComparison.OrdinalIgnoreCase)) {
                 return fullPath;
             }
 
-            return fullPath.Substring(rootPath.Length).TrimStart(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar);
+            return fullPath.Substring(rootPath.Length).TrimStart(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar).Replace("\\", "/");
         }
 
         public string GetFileSafeTimestamp() {
